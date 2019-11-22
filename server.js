c46547bb66062965bb936752198c27c312a289eb
SirMattCam <matthcameron@gmail.com>
2019-11-22 16:56:14 -0500

    trying to fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('./client/kanban-react/build'));
-app.use('/api/cards/', cardsRouter);
+//app.use('/api/cards/', cardsRouter);
 
 app.get('/*', (req, res) => {
   res.sendFile('index.html', {
