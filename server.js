02882472a9abb71ef899da4ce43609819f85f498
SirMattCam <matthcameron@gmail.com>
2019-12-06 09:18:40 -0500

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/api/cards/', cardsRouter);
 
 const path = require('path');
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
