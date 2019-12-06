61f7cc8b6523f4c3cde9554e217ca81f5f2f831a
SirMattCam <matthcameron@gmail.com>
2019-12-06 08:54:41 -0500

    build folder

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('./client/kanban-react/build'));
 app.use('/api/cards/', cardsRouter);
 
-const { PORT } = process.env;
+const path = require('path');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const { PORT } = process.env || 5001;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
--- server/db-conn.js
+++ server/db-conn.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose');
 const { DB_CONN, DB_USER, DB_PW } = process.env;
 
 mongoose
+  .connect(
+    'mongodb+srv://matt-cam_1:nakedpen3@kanbancluster-dajo3.azure.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => console.log('succesfully connected to DB (mongo on Azure)'))
+  .catch(console.error);
+/*
+THE INITIAL WAY THAT I HAD THIS SET UP
+mongoose
   .connect(DB_CONN, {
     auth: {
       user: DB_USER,
@@ -12,3 +24,4 @@ mongoose
   })
   .then(() => console.log('succesfully connected to DB (mongo on Azure)'))
   .catch(console.error);
+*/
