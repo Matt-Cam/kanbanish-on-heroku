b0088fc232cd0b74b03e62b20190e49f59a3f769
Matt Cameron <matthcameron@gmail.com>
2020-02-14 11:36:05 -0500

    env variable setup

--- server/db-conn.js
+++ server/db-conn.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose');
 const { DB_CONN, DB_USER, DB_PW } = process.env;
 
 mongoose
-  .connect(
-    'mongodb+srv://matt-cam_1:nakedpen3@kanbancluster-dajo3.azure.mongodb.net/test?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(DB_CONN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('succesfully connected to DB (mongo on Azure)'))
   .catch(console.error);
